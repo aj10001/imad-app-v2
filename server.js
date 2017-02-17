8f9aa24f026ebf945b614053ab6f8f2ab323279c
aj10001 <aj10001@gmail.com>
2017-02-17 22:35:04 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -13,8 +13,8 @@ app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 
-app.get('/ui/madi.png', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
+app.get('/ui/IMG_2145-01.jpeg', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'IMG_2145-01.jpeg'));
 });
 
 
